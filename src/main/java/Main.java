dc3430ff79f737afc295a39f56ad2f494a05bbb0
takumitak <161606155+takumitak@users.noreply.github.com>
2024-03-13 13:25:57 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,29 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("H");
+    System.out.println("e");
+    System.out.println("l");
+    System.out.println("l");
+    System.out.println("o");
+    System.out.println(" ");
+    System.out.println("w");
+    System.out.println("o");
+    System.out.println("l");
+    System.out.println("d");
+    System.out.println(" ");
+
+    String str = "Hello world";
+    for(int i = 0; i < str.length(); i++){
+      System.out.println(str.charAt(i));
+    }
+  }
+
+  // @Test
+  // void addition() {
+  //     assertEquals(2, 1 + 1);
+  // }
+}
